4524b87ad774ce049b8d9cbf78db37a1b99d6f25
Abner Teixeira <abner.ba81@gmail.com>
2021-12-29 20:28:33 +0000

    changed fonts

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ var image2 =  document.querySelectorAll("img")[1];
 image2.setAttribute ("src", randomImgSrc);
 
 if( randomNumber1 > randomNumber2){
-document.querySelector("h1").innerHTML = "LEFT SIDE WIN!!!";
+document.querySelector("h1").innerHTML = "LADO ESQUERDO GANHOU!!!";
 }
 else if ( randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = "RIGHT SIDE WIN!!";
+  document.querySelector("h1").innerHTML = "LADO DIREITO GANHOU!!";
 }else
-document.querySelector("h1").innerHTML = "DRAW";
+document.querySelector("h1").innerHTML = "EMPATOU";
